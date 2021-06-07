e5989cde173c8b5bf44a72a49cf713ae8c12485f
legendsovermyths <maskwolf2000@gmail.com>
2021-06-07 13:15:57 +0530

    first commit

---
+++ server/models/db.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+mongoose.connect(process.env.MONGODB_URI, (err) => {
+  if (!err) {
+    console.log("Connection established!");
+  } else {
+    console.log("Error: " + JSON.stringify(err, undefined, 2));
+  }
+});
+
+require("./user.model");
